6c2338d69f6abe6d7619705c136ce130704570bf
rreisnascimento <roger088reis@gmail.com>
2022-09-16 16:35:52 -0300

    Integrando com a API

--- src/components/sections/Parceiro/Parceiro.js
+++ src/components/sections/Parceiro/Parceiro.js
@@ -1,19 +1,44 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import api from './../../../service/api';
 import Section from '../../../HOC/Section';
 
 const Parceiro = () => {
+
+  const [nome, SetNome] = useState();
+  const [especialidade, SetEspecialidade] = useState();
+  const [endereco, SetEndereco] = useState();
+  const [complemento, SetComplemento] = useState();  
+  const [bairro, SetBairro] = useState();
+  const [cidade, SetCidade] = useState();
+  const [uf, SetUF] = useState();
+
+  async function Save() {
+   
+    await api.post(`/precadastro/create`, {
+      nome,
+      especialidade,
+      endereco,
+      complemento,
+      bairro,
+      cidade,
+      uf
+      }).then(() => {
+        console.log('Deu alguma BO aqui para gravação');
+      });    
+  };
+
+  useEffect(() => {
+  }, []);  
+
   return (
     <Section id='parceiro'>
       <div className='container pt-2 pb-5'>
         <div className='section-header pt-5 pb-5 text-center'>
           <h3 className='section-title'>
-            <span>Torne se um </span>Parceiro ConsultNew
+            <span>Torne se um </span>Parceiro
           </h3>
           <h6 className='section-subtitle mr-auto ml-auto'>
-            Individualized quality care that meets the total needs of the
-            patient Individualized quality care that quality care that
-            Individualized quality care that meets the total.
+            Seja um parceiro ConsultNew e ofereça a qualidade dos seus serviços a milhares de pessoas.
           </h6>
         </div>
         <div className='section-content'>
@@ -21,63 +46,36 @@ const Parceiro = () => {
             <div className='col-md-9 col-lg-7 mr-auto ml-auto'>
               <form>
                 <div className='form-group'>
-                  <input
-                      type='text'
-                      className='form-control rounded-0'
-                      aria-describedby='emailHelp'
-                      placeholder='Nome'
-                    />
+                  <input type='text' className='form-control rounded-0' placeholder='Nome do Profissinal'
+                      onChange={e => SetNome(e.target.value)} value={nome} />
                 </div>
-
                 <div className='form-group'>
-                  <input
-                      type='text'
-                      className='form-control rounded-0'
-                      aria-describedby='emailHelp'
-                      placeholder='Endereço'
-                    />
+                  <input type='text' className='form-control rounded-0' placeholder='Especialidade'
+                      onChange={e => SetEspecialidade(e.target.value)} value={especialidade} />
                 </div>
-
                 <div className='form-group'>
-                  <input
-                      type='text'
-                      className='form-control rounded-0'
-                      aria-describedby='emailHelp'
-                      placeholder='Bairro'
-                    />
+                  <input type='text' className='form-control rounded-0' placeholder='Endereço'
+                      onChange={e => SetEndereco(e.target.value)} value={endereco} />
                 </div>
-
                 <div className='form-group'>
-                  <input
-                      type='text'
-                      className='form-control rounded-0'
-                      aria-describedby='emailHelp'
-                      placeholder='Cidade'
-                    />
-                </div>                
-
+                  <input type='text' className='form-control rounded-0' placeholder='Complemento'
+                      onChange={e => SetComplemento(e.target.value)} value={complemento} />
+                </div>
                 <div className='form-group'>
-                  <input
-                      type='text'
-                      className='form-control rounded-0'
-                      aria-describedby='emailHelp'
-                      placeholder='Celular'
-                    />
-                </div>                
-
+                  <input type='text' className='form-control rounded-0' placeholder='Bairro'
+                      onChange={e => SetBairro(e.target.value)} value={bairro} />
+                </div>                                
                 <div className='form-group'>
-                  <input
-                      type='email'
-                      className='form-control rounded-0'
-                      aria-describedby='emailHelp'
-                      placeholder='email'
-                    />
-                </div>                
-
-
+                  <input type='text' className='form-control rounded-0' placeholder='Cidade'
+                      onChange={e => SetCidade(e.target.value)} value={cidade} />
+                </div>
+                <div className='form-group'>
+                  <input type='text' className='form-control rounded-0' placeholder='UF'
+                      onChange={e => SetUF(e.target.value)} value={uf} />
+                </div>
                 <div className='form-group text-center'>
-                  <button className='btn btn-block btn-primary rounded-0 mr-auto ml-auto'>
-                    Solicitar Cadastro
+                  <button className='btn btn-block btn-primary rounded-0 mr-auto ml-auto' onClick={Save}>
+                    Quero ser um Parceiro
                   </button>
                 </div>
               </form>
